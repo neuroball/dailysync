3e2927a5d00be650b0e959289306a6632a697663
neuroball <36119385+neuroball@users.noreply.github.com>
2022-05-27 23:28:08 -0700

    expanded on previous script in test environment.

--- dailysync.py
+++ dailysync.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import multiprocessing
+from multiprocessing import Pool
 import subprocess
 import os
 
@@ -8,18 +8,32 @@ src = "/data/prod/"
 dest = "/data/prod_backup/"
 events = []
 
+
 def copy_event(event):
+  print(list(event))
+  pass
   subprocess.call(list(event))
 
-for root, dirs, files in os.walk(src, topdown=False):
-  for name in files:
-    _src = os.path.join(root, name)
-    _dest = _src.replace(src, dest)
-    events.append( ("rsync", "-arq", _src, _dest) )
-  for name in dirs:
-    _src = os.path.join(root, name)
-    _dest = _src.replace(src, dest)
-    events.append( ("rsync", "-arq", _src, _dest) )
 
-  with Pool(len(events)) as p:
+def main():
+  src_path = os.getcwd() + src
+
+  for root, dirs, files in os.walk(src_path, topdown=False):
+    print(root, dirs, files)
+    for name in files:
+      _src = os.path.join(root, name)
+      _dest = _src.replace(src, dest)
+      events.append( ("rsync", "-arq", _src, _dest) )
+    for name in dirs:
+      _src = os.path.join(root, name)
+      _dest = _src.replace(src, dest)
+      events.append( ("rsync", "-arq", _src, _dest) )
+
+  print(events)
+  with Pool() as p:
     p.map(copy_event, events)
+
+
+if __name__ == "__main__":
+  main()
+
